ed951c2a17a4750b27a4ef73a1c319c5a4595ebe
Norbert <norbert.nytko@yahoo.com>
2015-10-24 14:14:17 +0200

    Add more configuration for OmniAuth
     * Create OmniauthCallbacksController
     * Add from_omniauth method to User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
-  devise_for :users
+
   resources :bars
+  resources :locals
+
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+
+  devise_scope :user do
+    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
 end
