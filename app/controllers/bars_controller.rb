cb20db7901e07bd4e5a72b086645f3c44e668a52
Kamil Obst <obst.kamil@gmail.com>
2015-10-24 14:43:53 +0200

    DRY code for Edit and Create Bar

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,24 +1,22 @@
 class BarsController < ApplicationController
   expose(:bars)
-	expose(:bar, attributes: :bar_params)
+  expose(:bar, attributes: :bar_params)
+  
+  def create
+    if bar.save
+      redirect_to bars_path(bar), notice: "Bar has been created"
+    else
+      render :new
+    end
+  end
 
-	def create
-		if bar.save
-			redirect_to bar
-		else
-			render 'new'
-		end
-	end
-
-	def update
-		if bar.save
-			redirect_to bar
-		else
-			render 'edit'
-		end
-	end
-
-	private
+  def update
+    if bar.save
+      redirect_to bars_path(bar), notice: "Bar has been updated"
+    else
+      render :edit
+    end
+  end
 
 	def bar_params
 		params.require(:bar).permit(:name, :lat, :lng)
--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,2 +1,4 @@
 class Bar < ActiveRecord::Base
+   validates :name, presence: true
 end
+
