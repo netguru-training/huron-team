85ce9aa2e584225a57d1625c143b0b3b563a5b75
Kamil Obst <obst.kamil@gmail.com>
2015-10-25 14:30:13 +0100

    Decorator - more readable code

--- app/decorators/bar_decorator.rb
+++ app/decorators/bar_decorator.rb
@@ -1,11 +1,11 @@
 class BarDecorator < BaseDecorator
 
   def name_with_count
-    "#{bar.name} (#{bar.beers.count})"
+    "#{name} (#{beers.count})"
   end
 
   def street_and_city
-    "#{bar.street} #{bar.city}"
+    "#{street} #{city}"
   end
 
 end
